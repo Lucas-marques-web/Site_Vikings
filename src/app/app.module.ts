a6ced1dbbfb5ecce55899544181bba0bf255c987
Lucas Paiva Marques <lp411301@gmail.com>
2021-06-10 16:47:08 -0300

    atualizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonagensComponent } from './personagens/personagens.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonagensComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
